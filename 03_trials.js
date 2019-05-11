6e4285b74868d6d3bb0b824a3cc3ed739f9af228
lschiesser <41335130+lschiesser@users.noreply.github.com>
2019-05-11 13:25:03 +0200

    added 2nd trial

--- 03_trials.js
+++ 03_trials.js
@@ -15,6 +15,17 @@ const trial_info = {
             number: 1,
             congruency: "different"
         },
-
+        {
+            question: "Same or different?",
+            picture: "images/1_50_same.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: "same",
+            j: "different",
+            expected: "same",
+            rotation: 50,
+            number: 2,
+            congruency: "same"
+        },
     ],
 };
