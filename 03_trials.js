c672dd89422b3b3ddaf7138974ed353c8588e9ed
parihumagain <49266833+parihumagain@users.noreply.github.com>
2019-05-11 15:37:41 +0200

    added one trail image 2_50 same

--- 03_trials.js
+++ 03_trials.js
@@ -27,5 +27,17 @@ const trial_info = {
             number: 2,
             congruency: "same"
         },
+        {
+            question: "Same or different?",
+            picture: "images/2_50_same.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: "same",
+            j: "different",
+            expected: "same",
+            rotation: 50,
+            number: 2,
+            congruency: "same"
+        },
     ],
 };
